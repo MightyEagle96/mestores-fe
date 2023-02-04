1f7f6d045b43af700ff289a9bb05bd9ad2eb2a14
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-02-04 13:00:38 +0100

    made changes

--- src/pages/public/ProductCard.jsx
+++ src/pages/public/ProductCard.jsx
@@ -1,22 +1,42 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, forwardRef } from "react";
 import {
   Card,
   CardMedia,
   CardContent,
   Typography,
   CardActions,
+  Button,
 } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { LoadingButton } from "@mui/lab";
 import { CartContext } from "../../context/CartContext";
 import { httpService } from "../../httpService";
+import Snackbar from "@mui/material/Snackbar";
+
+import MuiAlert from "@mui/material/Alert";
 
 export default function ProductCard(c) {
   const { cart, setCart } = useContext(CartContext);
-  console.log(cart);
+
   const [loading, setLoading] = useState(false);
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
   const addToCart = async (newItem) => {
     //setCart(() => [...cart, newItem]);
 
@@ -41,7 +61,8 @@ export default function ProductCard(c) {
 
         if (res2) {
           setLoading(false);
-          console.log(res2.data);
+          setCart(res.data.products);
+          handleClick();
         }
       }
 
@@ -53,47 +74,60 @@ export default function ProductCard(c) {
         { product: c._id }
       );
       if (res && res.data) {
+        handleClick();
         setCart(res.data.products);
       }
       setLoading(false);
     }
   };
   return (
-    <Card>
-      <CardMedia component="img" height="200" image={c.image} />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h6"
-          component="div"
-          fontWeight={600}
-          color="GrayText"
-        >
-          {c.name}
-        </Typography>
-        <Typography variant="subtitle2">{c.description}</Typography>
-      </CardContent>
-      <CardActions>
-        <Typography fontWeight={600} fontSize={23}>
-          ₦{c.price.toLocaleString()}
-        </Typography>
-        {cart.includes(c._id) ? (
-          <span className="p-3">
-            <Typography variant="">Added to cart</Typography>
-          </span>
-        ) : (
-          <span className="p-2">
-            <LoadingButton
-              loadingPosition="end"
-              onClick={addToCart}
-              loading={loading}
-              endIcon={<FontAwesomeIcon icon={faCartPlus} />}
-            >
-              <span> Add to cart</span>
-            </LoadingButton>
-          </span>
-        )}
-      </CardActions>
-    </Card>
+    <>
+      <Card>
+        <CardMedia component="img" height="200" image={c.image} />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            fontWeight={600}
+            color="GrayText"
+          >
+            {c.name}
+          </Typography>
+          <Typography variant="subtitle2">{c.description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Typography fontWeight={600} fontSize={23}>
+            ₦{c.price.toLocaleString()}
+          </Typography>
+          {cart.includes(c._id) ? (
+            <span className="p-3">
+              <Typography variant="">Added to cart</Typography>
+            </span>
+          ) : (
+            <span className="p-2">
+              <LoadingButton
+                loadingPosition="end"
+                onClick={addToCart}
+                loading={loading}
+                endIcon={<FontAwesomeIcon icon={faCartPlus} />}
+              >
+                <span> Add to cart</span>
+              </LoadingButton>
+            </span>
+          )}
+        </CardActions>
+      </Card>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Product added to your cart
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
