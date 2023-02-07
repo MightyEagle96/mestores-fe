6d7f219586d21287d11b955ba2b4daa041b58614
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-02-07 17:10:15 +0100

    made changes

--- src/pages/public/ConfirmOrder.jsx
+++ src/pages/public/ConfirmOrder.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function ConfirmOrder() {
-  return <div>ConfirmOrder</div>;
+  const { cartId } = useParams();
+
+  console.log(cartId);
+  return (
+    <div>
+      <div className="mt-5 mb-5 container">Hello</div>
+    </div>
+  );
 }
